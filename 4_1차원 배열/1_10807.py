25a181cb41cbb48a2bd46107fce5913f9dc64fdc
songseogyeong <0307ssk@naver.com>
2024-09-06 00:24:15 +0900

    day 13

---
+++ 4_1차원 배열/1_10807.py
@@ -0,0 +1,33 @@
+# 10807
+# 개수 세기
+
+# 문제
+# 총 N개의 정수가 주어졌을 때, 정수 v가 몇 개인지 구하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다.
+# 둘째 줄에는 정수가 공백으로 구분되어져있다.  
+# 셋째 줄에는 찾으려고 하는 정수 v가 주어진다. 입력으로 주어지는 정수와 v는 -100보다 크거나 같으며, 100보다 작거나 같다.
+
+# 출력
+# 첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
+
+
+# 풀이 1
+N = int(input())
+num = []
+
+for i in map(int, input().split()):
+  num.append(i)
+
+V = int(input())
+
+print(num.count(V))
+
+
+# 풀이 2
+N = int(input())
+num = list(map(int, input().split()))
+V = int(input())
+
+print(num.count(V))
