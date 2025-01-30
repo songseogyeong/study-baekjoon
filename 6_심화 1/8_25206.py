3b1fcf9793ad9a9bac3be371afe46f0f98c8759e
songseogyeong <0307ssk@naver.com>
2025-01-30 13:55:26 +0900

    day 43

--- 6_심화 1/8_25206.py
+++ 6_심화 1/8_25206.py
@@ -75,3 +75,21 @@ result = total_score / total_credit
 
 print(f"{result:.6f}")
 
+# 풀이 3
+grade = {"A+":4.5, "A0":4.0, "B+":3.5, "B0":3.0, "C+":2.5, "C0":2.0, "D+":1.5, "D0":1.0, "F":0.0}
+
+total_score = 0
+total_credit = 0
+
+for _ in range(20):
+  subject = list(input().split())
+
+  if subject[2] == "P":
+    continue
+  else:
+    total_score += float(subject[1]) * grade[subject[2]]
+    total_credit += float(subject[1])
+
+result = total_score / total_credit
+
+print(f"{result:.6f}")
